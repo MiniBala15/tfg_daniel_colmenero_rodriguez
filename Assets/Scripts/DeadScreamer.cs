4de569062098d355bc6e497b3a03a09e4a2833a1
MiniBala15 <danielcolme15@gmail.com>
2023-06-10 19:29:03 +0200

    now the enemy can kill the character, changed main menu

---
+++ Assets/Scripts/DeadScreamer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadScreamer : MonoBehaviour
+{
+    public GameObject[] jumpScare;
+    private float delayInSeconds = 3f;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            Instantiate(jumpScare[Random.Range(0, jumpScare.Length)]);
+            Invoke("changeScene", delayInSeconds);
+        }    
+    }
+
+    public void changeScene() {
+        SceneManager.LoadScene(0);
+    }
+}
